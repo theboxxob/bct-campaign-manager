d71e65ae4c77f818cbf106d051570c05b90c2f17
boxxob <8qhosuzyk@mozmail.com>
2023-06-16 16:32:15 +0300

    Improved existing test cases to be ran without using subprocesses

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,27 +4,22 @@ import json
 import shutil
 from pathlib import Path
 
-def add_campaign():
-    subprocess.run(
-        ["python3", "main.py", "campaign", "add", "test_campaign"], check=True
-    )
+from core import add_campaign, add_participant, remove_participant
 
-def add_participant():
-    subprocess.run(
-        ["python3", "main.py", "campaign", "add_participant", "test_campaign", "3"], check=True
-    )
+class Namespace:
+    """Class to mimic argparse namespace"""
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
 
-def remove_participant():
-    subprocess.run(
-        ["python3", "main.py", "campaign", "remove_participant", "test_campaign", "3"], check=True
-    )
 
 def get_metadata(p):
+    """Load the file containing campaign metadata as JSON and return it"""
     with p.open('r') as f:
         return json.load(f)
 
-class CampaignTestCase(unittest.TestCase):
 
+class CampaignTestCase(unittest.TestCase):
+    """Tests campaign related commands"""
     def setUp(self):
         self.campaign_path = Path('campaigns/test_campaign')
         self.metadata_path = self.campaign_path / 'metadata.json'
@@ -37,21 +32,27 @@ class CampaignTestCase(unittest.TestCase):
             shutil.rmtree(self.campaign_path)
 
     def test_add_campaign(self):
-        add_campaign()
+        """Test adding campaign"""
+        ns = Namespace(campaign_name='test_campaign', data_folder=None)
+        add_campaign(ns)
         self.assertTrue(self.campaign_path.is_dir())
         self.assertTrue(self.metadata_path.is_file())
         metadata = get_metadata(self.metadata_path)
         self.assertTrue(metadata.get('campaign_name') == 'test_campaign')
 
     def test_add_participant(self):
-        add_campaign()
-        add_participant()
+        """Test adding participant to campaign"""
+        ns = Namespace(campaign_name='test_campaign', uid=3, data_folder=None)
+        add_campaign(ns)
+        add_participant(ns)
         metadata = get_metadata(self.metadata_path)
         self.assertTrue(metadata.get('participants').get('3') == 'satoshi')
 
     def test_remove_participant(self):
-        add_campaign()
-        add_participant()
-        remove_participant()
+        """Test removing participant from campaign"""
+        ns = Namespace(campaign_name='test_campaign', uid=3, data_folder=None)
+        add_campaign(ns)
+        add_participant(ns)
+        remove_participant(ns)
         metadata = get_metadata(self.metadata_path)
         self.assertEqual(None, metadata.get('participants').get('3'))
